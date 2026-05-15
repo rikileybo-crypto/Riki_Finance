82915746f757fd3bc8dde1acd3bce79b9e1820d6
Riki Leybowitz <riki@jrmedia.co.il>
2026-05-15 13:23:47 +0300

    Fix Supabase URL typo

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://luprxzpmhvsrggnibijup.supabase.co',
+  'https://luprxzpmhvsrggnibjup.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1cHJ4enBtaHZzcmdnbmlianVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MjQ5MDksImV4cCI6MjA5NDQwMDkwOX0.9vbpmGnEhOAiCOZMXDr6xGpNpbGM4brkHUysOWaYtIo'
 );
