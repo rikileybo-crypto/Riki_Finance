c06f2d347614f99fcd8154b60800d8ecc85c8172
rikileybo-crypto <rikileybo@gmail.com>
2026-05-17 11:01:19 +0300

    Fix: remove () from getSupabase import in sync.js

--- backend/src/routes/sync.js
+++ backend/src/routes/sync.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
-import { getSupabase } from '../services/getSupabase().js';
+import { getSupabase } from '../services/getSupabase.js';
 import { decrypt } from '../services/encryption.js';
 import { scrapeAccount } from '../services/scraper.js';
 import { checkAlerts } from '../services/alertChecker.js';
